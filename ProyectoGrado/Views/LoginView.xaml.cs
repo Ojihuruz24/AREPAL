4bf3cf4e950f6bc4e7a1fe186e6668c472578d99
dayan patiño <dayan.agudelo.cetad@epfac.edu.co>
2020-10-01 10:14:42 -0500

    Add: Inicio del proyecto

---
+++ ProyectoGrado/Views/LoginView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using ProyectoGrado.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ProyectoGrado.Views
+{
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            ((LoginViewModel)DataContext).Password = ((PasswordBox)sender).SecurePassword;
+        }
+    }
+}
