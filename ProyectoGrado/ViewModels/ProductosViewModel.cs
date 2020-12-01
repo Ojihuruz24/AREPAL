db28083a2216fe107d27486ccf0fddbc489e015b
dayan patiño <dayan.agudelo.cetad@epfac.edu.co>
2020-11-30 19:12:37 -0500

    Update: actualiza la tabla de productos por una observableCollection para por utilizar el filtro de busqueda.

--- ProyectoGrado/ViewModels/ProductosViewModel.cs
+++ ProyectoGrado/ViewModels/ProductosViewModel.cs
@@ -1,16 +1,22 @@
 ﻿using Prism.Mvvm;
+using ProyectoGrado.Models;
 using ProyectoGrado.Utility.Validations;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
+using System.Windows.Data;
 
 namespace ProyectoGrado.ViewModels
 {
     public class ProductosViewModel : BindableBase
     {
-        private DataTable _products;
-        private string _searchProduct;
+        private ObservableCollection<Producto> _products;
+        private string _searchProduct = "";
+        private ICollectionView _filteredSearch;
 
-        public DataTable Products
+        public ObservableCollection<Producto> Products
         {
             get { return _products; }
             set { SetProperty(ref _products, value); }
@@ -21,50 +27,81 @@ namespace ProyectoGrado.ViewModels
             get { return _searchProduct; }
             set
             {
-                if (ValidationesInput.IsNumber(value, "Codigo invalido"))
+                if (_searchProduct != value)
                 {
                     SetProperty(ref _searchProduct, value);
-                    Search(value);
+                    _filteredSearch.Refresh(); 
                 }
-
-
             }
         }
 
         public ProductosViewModel()
         {
+            Products = new ObservableCollection<Producto>();
+            SetFilteredProvider();
             ConectionTable();
         }
 
-        private void Search(string search)
+
+        public void SetFilteredProvider()
         {
-            if (!string.IsNullOrEmpty(search))
+            _filteredSearch = CollectionViewSource.GetDefaultView(Products);
+            _filteredSearch.Filter = Filter;
+        }
+
+        private bool Filter(object obj)
+        {
+            if (obj is Producto prodcuto)
             {
-                Products.DefaultView.RowFilter = $"CODIGO = {SearchProduct}";
-                return;
+                var search = $"{prodcuto.Codigo} {prodcuto.Nombre} {prodcuto.Categoria}";
+
+                return search.ToLower().Contains(SearchProduct.ToLower());
             }
 
-            ConectionTable();
+            return false;
         }
+
 
         private void ConectionTable()
         {
-            DataTable dt = new DataTable();
             using (var conn = new SqlConnection(LoginViewModel.ConectionBD))
             {
-                string query = "SELECT " +
-                    " PRODUCTO.ID," +
-                    " PRODUCTO.CODIGO," +
-                    " PRODUCTO.NOMBRE ," +
-                    " PRODUCTO.PRECIO, " +
-                    " CATEGORIA.NOMBRE AS CATEGORIA" +
-                    " FROM CATEGORIA " +
-                    "INNER JOIN PRODUCTO ON CATEGORIA.ID = PRODUCTO.ID_CATEGORIA";
+                try
+                {
+                    string query = "SELECT " +
+                               " PRODUCTO.ID," +
+                               " PRODUCTO.CODIGO," +
+                               " PRODUCTO.NOMBRE ," +
+                               " PRODUCTO.PRECIO, " +
+                               " CATEGORIA.NOMBRE AS CATEGORIA" +
+                               " FROM CATEGORIA " +
+                               "INNER JOIN PRODUCTO ON CATEGORIA.ID = PRODUCTO.ID_CATEGORIA";
 
-                SqlCommand cmd = new SqlCommand(query, conn);
-                SqlDataAdapter da = new SqlDataAdapter(cmd);
-                da.Fill(dt);
-                Products = dt;
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Products.Clear();
+
+                    while (reader.Read())
+                    {
+
+                        Products.Add(new Producto
+                        {
+                            Codigo = reader.GetString(1),
+                            Nombre = reader.GetString(2),
+                            Precio = reader.GetInt32(3),
+                            Categoria = reader.GetString(4)
+                        });
+
+                    }
+
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    conn.Close();
+                }
             }
         }
     }
